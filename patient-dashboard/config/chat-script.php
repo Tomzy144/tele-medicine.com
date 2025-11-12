23181fa5aaf611e62991775f5c52edd5ea718835
Tomzy.cs,Vb <oyemadetomiwa@gmail.com>
2025-11-12 17:26:29 +0100

    "updated"

--- patient-dashboard/config/chat-script.php
+++ patient-dashboard/config/chat-script.php
@@ -49,7 +49,7 @@ function refreshChat() {
 
     function connectWebSocket() {
      const wsUrl = window.location.hostname === "localhost"
-        ? "ws://localhost:8082"
+        ? "ws://tele-medicine-serverjs"
         : "wss://tele-medicine-chat-server.onrender.com";
 
 
